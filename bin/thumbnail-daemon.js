9c535ccdd42f2acfd306d2ff9189d1b39d0c1bb2
Pan Zhiyong <pan.zhiyong@grandlynn.com>
2019-08-30 17:40:02 +0800

    fix: can not determine imagemagick command. fix: do not response to created file.

--- bin/thumbnail-daemon.js
+++ bin/thumbnail-daemon.js
@@ -4,15 +4,9 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const path = require('path');
 
-let MAGICK_CMD = null;
-
-determineMagickCmd().then(cmd => {
-  console.log('determined ImageMagick command name:', cmd);
-  MAGICK_CMD = cmd;
-}).catch(error => {
-  console.error('Error: failed to determine ImageMagick command name, have you installed it?');
-  process.exit(1);
-});
+// 在 Linux 上，convert 命令就是 "convert"，而在 Windows 上，convert 命令是
+// "magick convert"。
+let MAGICK_CMD = process.env.MAGICK_CMD || "magick convert";
 
 let debouncedGenThumbnail = _.debounce(genThumbnail, 1000);
 
@@ -25,6 +19,7 @@ watcher = hound.watch(process.argv[2]);
  
 watcher.on('create', function(file, stats) {
   console.log(file + ' was created');
+  debouncedGenThumbnail(file);
 })
 
 watcher.on('change', function(file, stats) {
@@ -39,25 +34,9 @@ watcher.on('delete', function(file) {
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
- * 在 Linux 上，convert 命令就是 "convert"，而在 Windows 上，convert 命令是
- * "magick convert"。
- *
- * 这个函数会返回当前系统上可用的命令。
- */
-function determineMagickCmd() {
-  let cmd = "(type magick >/dev/null 2>&1 && echo 'magick convert') || (type convert >/dev/null 2>&1 && echo 'convert')";
-  return exec(cmd).then(o => o.stdout.trim());
-}
-
-/**
  * For a given file, if it's an image, create it's thumbnail as <file>.thumbnail.jpg
  */
 function genThumbnail(file) {
-  if (!MAGICK_CMD) {
-    console.warn('MAGICK_CMD is not determined.');
-    return;
-  }
-
   // ignore tmp file (startsWith '~')
   if (path.basename(file).startsWith('~')) {
     return;
@@ -67,6 +46,8 @@ function genThumbnail(file) {
     console.log(file + ' thumbnail...');
     let thumbnail = file + '.thumbnail.jpg';
     let cmd = `${MAGICK_CMD} -thumbnail 100x100 "${file}" "${thumbnail}"`; 
-    exec(cmd);
+    exec(cmd).catch(error => {
+      console.error(error);
+    });
   }
 }
